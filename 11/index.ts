cc5d91a0d30964ef7b7679746836132040e641c4
lldan <safinildan@gmail.com>
2023-10-30 10:34:48 +0300

    15

--- 11/index.ts
+++ 11/index.ts
@@ -3,11 +3,11 @@ function maxArea(height: number[]): number {
   let right = height.length - 1;
   let mm = -1;
   while (left !== right) {
-    mm = Math.max(mm, (right - left) * Math.max(height[right], height[left]));
+    mm = Math.max(mm, (right - left) * Math.min(height[right], height[left]));
     if (height[left] < height[right]) {
       left++;
     } else if (height[left] > height[right]) {
-      right++;
+      right--;
     } else {
       left++;
     }
