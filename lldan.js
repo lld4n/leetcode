6c6ed50fa00ce9a6d12a55470194d1ef070d860e
lldan <safinildan@gmail.com>
2023-11-07 01:48:08 +0300

    да

--- lldan.js
+++ lldan.js
@@ -1,5 +1,31 @@
 import fs from "fs";
 import matter from "gray-matter";
+import { exec } from "child_process";
+const currentTimestamp = new Date().toLocaleString("en-US", {
+  dateStyle: "medium",
+  timeStyle: "short",
+});
+exec("git add .", (err, stdout, stderr) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(stdout);
+});
+exec(`git commit -m "${currentTimestamp}"`, (err, stdout, stderr) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(stdout);
+});
+exec("git push", (err, stdout, stderr) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(stdout);
+});
 const throws = [
   "-1",
   ".git",
@@ -13,3 +39,6 @@ const throws = [
   "yarn.lock",
   ".DS_Store",
 ];
+
+const dir = fs.readdirSync("./");
+console.log(dir);
