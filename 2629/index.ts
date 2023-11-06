ea3537fff8f34350f210cb1053a5fccd2066f582
lldan <safinildan@gmail.com>
2023-11-06 05:28:56 +0300

    2721

--- 2629/index.ts
+++ 2629/index.ts
@@ -1,15 +1,15 @@
 // type F = (x: number) => number;
 
-function compose(functions: F[]): F {
-  return function (x) {
-    if (functions.length === 0) return x;
-    let res = x;
-    for (let fn of functions.reverse()) {
-      res = fn(res);
-    }
-    return res;
-  };
-}
+// function compose(functions: F[]): F {
+//   return function (x) {
+//     if (functions.length === 0) return x;
+//     let res = x;
+//     for (let fn of functions.reverse()) {
+//       res = fn(res);
+//     }
+//     return res;
+//   };
+// }
 
 /**
  * const fn = compose([x => x + 1, x => 2 * x])
--- 2637/2637.ts
+++ 2637/2637.ts
@@ -1,23 +1,23 @@
-type Fn = (...params: any[]) => Promise<any>;
-
-function timeLimit(fn: Fn, t: number): Fn {
-  return (...params): Promise<any> => {
-    return new Promise<any>((resolve, reject) => {
-      const timer = setTimeout(() => {
-        reject("Time Limit Exceeded");
-      }, t);
-      fn(...params)
-        .then((res) => {
-          clearTimeout(timer);
-          resolve(res);
-        })
-        .catch((err) => {
-          clearTimeout(timer);
-          reject(err);
-        });
-    });
-  };
-}
+// // type Fn = (...params: any[]) => Promise<any>;
+//
+// function timeLimit(fn: Fn, t: number): Fn {
+//   return (...params): Promise<any> => {
+//     return new Promise<any>((resolve, reject) => {
+//       const timer = setTimeout(() => {
+//         reject("Time Limit Exceeded");
+//       }, t);
+//       fn(...params)
+//         .then((res) => {
+//           clearTimeout(timer);
+//           resolve(res);
+//         })
+//         .catch((err) => {
+//           clearTimeout(timer);
+//           reject(err);
+//         });
+//     });
+//   };
+// }
 
 /**
  * const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
