2c907bd13f0d7d8c7e446c4d39c500fab96cf2c6
lldan <safinildan@gmail.com>
2024-02-06 12:19:12 +0300

    6 февр. 24 г., 12:19

--- src/stats.js
+++ src/stats.js
@@ -4,12 +4,12 @@ exports.generateStats = void 0;
 var constants_1 = require("./constants");
 function generateStats() {
     var res = "<div align='center'>";
-    res += "<img src=\"".concat(constants_1.stats.wakatime, "\"><br>");
+    res += "<img src=\"".concat(constants_1.stats.wakatime, "\"><br><br>");
     for (var _i = 0, _a = constants_1.stats.badges; _i < _a.length; _i++) {
         var badge = _a[_i];
         res += "<img src=\"".concat(badge, "\" width=\"50\" height=\"50\">  ");
     }
-    res += "<br><img src=\"".concat(constants_1.stats.leetcode, "\"></div>");
+    res += "<br><br><img src=\"".concat(constants_1.stats.leetcode, "\"></div>");
     return res;
 }
 exports.generateStats = generateStats;
--- src/stats.ts
+++ src/stats.ts
@@ -2,10 +2,10 @@ import { stats } from "./constants";
 
 export function generateStats() {
   let res = "<div align='center'>";
-  res += `<img src="${stats.wakatime}"><br>`;
+  res += `<img src="${stats.wakatime}"><br><br>`;
   for (const badge of stats.badges) {
     res += `<img src="${badge}" width="50" height="50">  `;
   }
-  res += `<br><img src="${stats.leetcode}"></div>`;
+  res += `<br><br><img src="${stats.leetcode}"></div>`;
   return res;
 }
