0c303cf740bd8119bf3abf9e6169a79a3cf19b2b
lldan <safinildan@gmail.com>
2024-02-06 11:17:48 +0300

    6 февр. 24 г., 11:17

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,8 @@ import * as fs from "fs";
 import { spawnSync } from "child_process";
 
 let result = `<h1 align="center">неважно, возможно это или нет</h1>`;
-result += `<h2 align="center">я это сделаю, потому что хочу сделать</h2>`;
-result += `<h3 align="center">モンキー･D･ルフィ</h3>`;
+result += `<p align="center">я это сделаю, потому что хочу сделать</p>`;
+result += `<code align="center">モンキー･D･ルフィ</code>`;
 result += generateStats();
 fs.writeFileSync("README.md", result);
 const currentTimestamp = new Date().toLocaleString("RU-ru", {
