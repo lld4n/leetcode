9e27a350cce2760582d502597641e7e584d2c089
lldan <safinildan@gmail.com>
2024-02-19 16:44:14 +0300

    19 февр. 24 г., 16:44

--- src/constants.js
+++ src/constants.js
@@ -11,6 +11,7 @@ exports.folders = [
 exports.stats = {
     wakatime: "https://wakatime.com/badge/user/018afba7-2ebc-4282-8545-d0250012991b/project/018b347c-0b9d-4d3e-9cc5-745948186d06.svg",
     badges: [
+        "https://assets.leetcode.com/static_assets/marketing/2024-50-lg.png",
         "https://assets.leetcode.com/static_assets/marketing/lg50.png",
         "https://leetcode.com/static/images/badges/dcc-2024-1.png",
         "https://leetcode.com/static/images/badges/dcc-2023-12.png",
