6c8b1712bc9924a051d52f81ab70b4ff42be242e
lldan <safinildan@gmail.com>
2024-02-24 14:59:58 +0300

    24 февр. 24 г., 14:59

--- src/leetcode.ts
+++ src/leetcode.ts
@@ -1,11 +1,13 @@
 import LeetCode from "leetcode-query";
 import * as fs from "fs";
 
-async function generateDivergency() {
+export async function generateDivergency(count: number) {
   const leetcode = new LeetCode();
   const user = await leetcode.user("lldan");
-  console.log(user);
+  if (user.matchedUser) {
+    console.log(user.matchedUser?.submitStats.acSubmissionNum);
+  }
   fs.writeFileSync("./user.json", JSON.stringify(user));
 }
 
-generateDivergency();
+generateDivergency(1);
