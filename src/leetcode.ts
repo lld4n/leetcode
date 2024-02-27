258b7992e2c1e64f36a42837438092e7bd5a0856
lldan <safinildan@gmail.com>
2024-02-27 22:02:54 +0300

    27 февр. 24 г., 22:02

--- src/leetcode.ts
+++ src/leetcode.ts
@@ -1,12 +1,27 @@
 import LeetCode from "leetcode-query";
 
 export async function getLeetcode() {
-  _get();
+  const stats = await _get();
+  console.log(stats);
 }
 
 export async function _get() {
   const leetcode = new LeetCode();
   const l = await leetcode.user("lldan");
-  console.log(l.allQuestionsCount);
-  // console.log(user.matchedUser?.submitStats);
+
+  const map = {
+    All: [0, 0],
+    Easy: [0, 0],
+    Medium: [0, 0],
+    Hard: [0, 0],
+  };
+  if (l.matchedUser) {
+    for (const item of l.matchedUser.submitStats.acSubmissionNum) {
+      map[item.difficulty][0] = item.count;
+    }
+  }
+  for (const item of l.allQuestionsCount) {
+    map[item.difficulty][1] = item.count;
+  }
+  return map;
 }
