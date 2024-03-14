04a38b6ad09f6b658a866516149cb4b02e2418c5
lldan <safinildan@gmail.com>
2024-03-14 08:32:05 +0300

    14 мар. 24 г., 8:32

---
+++ март.24/14-03-24/930.ts
@@ -0,0 +1,12 @@
+function numSubarraysWithSum(nums: number[], goal: number): number {
+  let curr = 0;
+  let ans = 0;
+  const map = new Map<number, number>();
+  map.set(0, 1);
+  for (const el of nums) {
+    curr += el;
+    ans += map.get(curr - goal) || 0;
+    map.set(curr, (map.get(curr) || 0) + 1);
+  }
+  return ans;
+}
