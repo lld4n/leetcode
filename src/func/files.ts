bd9e6d1a44c94acb238fe57f93d77f5cee92e46d
lldan <safinildan@gmail.com>
2024-04-02 23:02:53 +0300

    2 апр. 24 г., 23:02

--- src/func/files.ts
+++ src/func/files.ts
@@ -1,6 +1,19 @@
 import { readdir } from "node:fs/promises";
 
+const list = [
+  "октябрь.23",
+  "апрель.24",
+  "другое",
+  "февраль.24",
+  "январь.24",
+  "ноябрь.23",
+  "декабрь.23",
+  "март.24",
+];
+
 export async function files() {
-  const files = await readdir(".");
-  console.log(files);
+  for (const item of list) {
+    const files = await readdir(`./${item}`, { recursive: true });
+    console.log(files);
+  }
 }
