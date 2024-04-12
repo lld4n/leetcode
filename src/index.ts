f703274286fecad28f82cee8085b920bd47ab854
lldan <safinildan@gmail.com>
2024-04-12 12:07:57 +0300

    12 апр. 24 г., 12:07

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,16 @@ async function main() {
   const start = new Date().getTime();
 
   let result = "";
+  console.log("пошла возня");
   result += badges();
+  console.log("badges");
   // result += await waka();
   result += await leetcode();
+  console.log("leetcode");
   result += await files();
-
+  console.log("files");
   result += await wakaMini();
+  console.log("wakatime");
   Bun.write("./README.md", result);
   sync();
   const end = new Date().getTime();
