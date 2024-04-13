e1efa01d21fccfdcaa8017b9ab9dd68b1fb0b10d
lldan <safinildan@gmail.com>
2024-04-13 22:34:39 +0300

    13 апр. 24 г., 22:34

--- src/func/days.ts
+++ src/func/days.ts
@@ -1,16 +1,19 @@
 import { readdir } from "node:fs/promises";
 import { FOLDERS } from "../constants.ts";
+import { complete, percentage, strip } from "./utils.ts";
 
 export async function days() {
   const map: { [key: string]: number } = {};
+  let c = 0;
   for (const top of FOLDERS) {
     const medium = await readdir("./" + top);
     for (const bottom of medium) {
       try {
-        const tasks = await readdir("./" + top + "/" + bottom);
-        map[top + "/" + bottom] = tasks.filter(
-          (e) => e !== ".DS_Store",
-        ).length;
+        const tasks = (
+          await readdir("./" + top + "/" + bottom)
+        ).filter((e) => e !== ".DS_Store");
+        map[top + "/" + bottom] = tasks.length;
+        c += tasks.length;
       } catch (e) {
         //
       }
@@ -20,15 +23,14 @@ export async function days() {
   const entries = Object.entries(map)
     .sort((a, b) => b[1] - a[1])
     .slice(0, 10);
-  console.log(entries);
-  // let res = "**files**\n```text\n";
-  // for (const item of entities) {
-  //   res += complete(extension2name[item[0]] || item[0]);
-  //   res += complete(item[1] + " files");
-  //   res += strip(item[1], c);
-  //   res += complete(percentage(item[1], c));
-  //   res += "\n";
-  // }
-  // res += "```\n\n";
-  return "";
+  let res = "**days**\n```text\n";
+  for (const item of entries) {
+    res += complete(item[0]);
+    res += complete(item[1] + " tasks");
+    res += strip(item[1], c);
+    res += complete(percentage(item[1], c));
+    res += "\n";
+  }
+  res += "```\n\n";
+  return res;
 }
