b2a2922b4dbe8358cd8e49959cafb5008528c72c
lldan <safinildan@gmail.com>
2024-10-17 20:19:43 +0300

    17 окт. 24 г., 20:19

---
+++ 24/октябрь.24/17-10-24/670.go
@@ -0,0 +1,28 @@
+package _7_10_24
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func maximumSwap(num int) int {
+	numStr := []rune(fmt.Sprint(num))
+	n := len(numStr)
+	last := make([]int, 10)
+
+	for i := 0; i < n; i++ {
+		last[numStr[i]-'0'] = i
+	}
+
+	for i := 0; i < n; i++ {
+		for d := 9; d > int(numStr[i]-'0'); d-- {
+			if last[d] > i {
+				numStr[i], numStr[last[d]] = numStr[last[d]], numStr[i]
+				result, _ := strconv.Atoi(string(numStr))
+				return result
+			}
+		}
+	}
+
+	return num
+}
