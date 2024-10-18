3807e32bd5392bf6c91161ee7bcd42483053e85d
lldan <safinildan@gmail.com>
2024-10-18 19:59:32 +0300

    18 окт. 24 г., 19:59

---
+++ 24/октябрь.24/19-10-24/2044.go
@@ -0,0 +1,20 @@
+package _9_10_24
+
+func countMaxOrSubsets(nums []int) int {
+	target, ans := 0, 0
+	for _, num := range nums {
+		target |= num
+	}
+
+	var backtrack func(int, int)
+	backtrack = func(start, cur int) {
+		if cur == target {
+			ans++
+		}
+		for i := start; i < len(nums); i++ {
+			backtrack(i+1, cur|nums[i])
+		}
+	}
+	backtrack(0, 0)
+	return ans
+}
